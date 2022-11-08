0c25eb74a2d41081d1428b76293f478a4ebe3202
vikrant29k <116786939+vikrant29k@users.noreply.github.com>
2022-11-08 12:32:12 +0530

    date expired or not

--- src/app/pages/view-log/component/logs/logs.component.ts
+++ src/app/pages/view-log/component/logs/logs.component.ts
@@ -27,9 +27,8 @@ export interface LogsData {
   
 })
 export class LogsComponent implements OnInit, AfterViewInit  {
-// serverdata=this.http.get('https://redbricks-server.herokuapp.com/logs/proposal-log');
-
- change: boolean=true;
+ change: boolean=false;
+ 
  proposalExtraDetailForm = new FormGroup({
 
   'change': new FormControl('')
@@ -39,20 +38,10 @@ export class LogsComponent implements OnInit, AfterViewInit  {
 
   }
   ngOnInit(): void {
+   
     this.getlog();
-  //   this.serverdata.subscribe((result:any)=>{
-  //     let newdata = result[0];
-  
-  // console.log(result);
-  // console.log(newdata);
-  
-    // });
-    
-    // this.http.get('https://redbricks-server.herokuapp.com/logs/proposal-log').subscribe((result: any) => {
-    //   this.dataSource = result;
-    //   console.log('DataSource::',this.dataSource);
-    //   console.log('Result::',result);
-    // })
+    this.sorted();
+
   }
   Logs:any;
   // let ELEMENT_DATA: PeriodicElement[] = [
@@ -82,28 +71,59 @@ export class LogsComponent implements OnInit, AfterViewInit  {
 
 
 getlog(){
+  
   this.http.get('https://redbricks-server.herokuapp.com/logs/proposal-log').subscribe((result: any) => {
-      this.tableDataSource(result);
-      console.log('Result::',result);
+    result.forEach((element:any) => {
+      console.log(element.createdAt);
+      const date =  new Date(element.createdAt);
+      const current = new Date();
+      var diff = current.getTime() - date.getTime(); 
+      var daydiff = diff / (1000 * 60 * 60 * 24); 
+        console.log("difference",daydiff);
+       
+        
+     if(daydiff>90)
+     {
+      this.change=true;
+      console.log("expired");
+     }else{
+      this.change=false;
+       console.log(" not expired");
+     }    
     });
-  // this.serverdata.subscribe((result:any)=>{
-  //   this.Logs = result;
-  //   console.log(result);
-  //   console.log(this.Logs);
-  //   this.tableDataSource(this.Logs);
-  // })
+    for(let i=0;i<result.length;i++)
+    {
+      if(this.change==false){
+        result[i].createdAt+="Not expired";
+      }else{
+        result[i].createdAt+="Expired";
+      }
+    }
+    this.tableDataSource(result);
+      
+     // console.log('Result::',result);
+    });
 }
+
+changecolor(val: boolean){
+  val=this.change;
+  if(val==false){
+    return false;
+  }else{
+    return true;
+  }
+
+}
+
 tableDataSource(data: any) {
   this.dataSource = new MatTableDataSource(data);
   this.dataSource.paginator = this.paginator;
-  console.log(this.dataSource);
- 
+
+  this.dataSource.sort = this.sort;
   // this.table.renderRows();
 }
   ngAfterViewInit() {
-    // this.dataSource.paginator = this.paginator;
-    // this.dataSource.sort = this.sort;
-   
+    
 }
 
   applyFilter(event: Event) {
@@ -119,38 +139,32 @@ tableDataSource(data: any) {
     }
   }
 
-  // getExpiredOrNot = (currentElementdate: string,i: any) => {
-  //   let current = new Date();
-  //   let currnetMonth = current.getMonth();
-  //   let currentDate = current.getDate();
-  //   let elementDate = new Date(currentElementdate);
-  //     let month = elementDate.getMonth();
-  //     let date = elementDate.getDate();
-  //     console.group(i);
-  //     console.log("Date:",currentDate);
-  //     console.log(current.toLocaleDateString())
-  //     console.log(elementDate.toLocaleDateString())
-  //     if((currnetMonth -2) >= month && currentDate > date){
-  //       console.log(true)
-  //       console.groupEnd();
-  //       return true
-  //     }
-  //     else{
-  //       console.log(false)
-  //       console.groupEnd();
-  //       return false
-  //     }
-  //   console.log(ELEMENT_DATA);
-  // }
-
   sorted(){
-//     const current = new Date();
+    this.http.get('https://redbricks-server.herokuapp.com/logs/proposal-log').subscribe((GetDate:any)=>{
+      // GetDate.forEach((element:any) => {
+      //   console.log(element.createdAt);
+      //   const date =  new Date(element.createdAt);
+      //   const current = new Date();
+     
+      //   var diff = current.getTime() - date.getTime(); 
+      //   var daydiff = diff / (1000 * 60 * 60 * 24); 
+      //     console.log("difference",daydiff);
+      //  if(daydiff>90)
+      //  {
+      //    console.log("expired");
+         
+      //  }else{
+ 
+      //    console.log(" not expired");
+      //  }
+      // });
+   
+   
+      // console.log(GetDate.crea)
     
-//     const numberOfDaysToSubstract = 90; //Dates to be subtracted
-//     const expiry = new Date().setDate(current.getDate() - numberOfDaysToSubstract);  //current date - 90 days
-//     const dateprint = new Date(expiry).getMonth(); 
-//     console.log(dateprint);
+    });
 
+  }
 //     for(let i=0;i<this.dataSource.data.length;i++){
 
 //       if ( new Date(this.dataSource.data[i].date).getMonth() <= dateprint ) {
@@ -166,6 +180,130 @@ tableDataSource(data: any) {
 //         this.change=false;
       
 //       }
-//   }
   }
-}
+  
+// }
+// import { Component, OnInit } from '@angular/core';
+// import { ViewChild } from '@angular/core';
+// import { MatCell, MatTableDataSource } from '@angular/material/table';
+// import { MatPaginator } from '@angular/material/paginator';
+// import {LiveAnnouncer} from '@angular/cdk/a11y';
+// import {AfterViewInit} from '@angular/core';
+// import {MatSort, Sort} from '@angular/material/sort';
+// import { FormControl, FormGroup } from "@angular/forms";
+// import { HttpClient } from '@angular/common/http';
+// export interface PeriodicElement {
+//   name: string;
+//     id: string;
+//     createdate: string;
+//     updatedate:string;
+//     action: string;
+//     message: string;
+ 
+// }
+
+// let ELEMENT_DATA: PeriodicElement[] = [
+//   {id:'RBOHYDSA02111145' , name: 'Aditya', createdate:new Date('01/01/2022').toString(),updatedate:new Date().toString(),action:'viewproposal',message:"Client info"},
+//   {id:'RBOHYDSA02111245' , name: 'Atul', createdate: new Date('09/01/2022').toString(),updatedate:new Date().toString(),action:'viewproposal',message:"Requirement"},
+ 
+// ];
+
+// @Component({
+//   selector: 'app-logs',
+//   templateUrl: './logs.component.html',
+//   styleUrls: ['./logs.component.scss']
+  
+// })
+// export class LogsComponent implements OnInit, AfterViewInit  {
+//  change: boolean=true;
+//  proposalExtraDetailForm = new FormGroup({
+
+//   'change': new FormControl('')
+// });
+
+//   constructor(private _liveAnnouncer: LiveAnnouncer, private http: HttpClient){
+
+//   }
+//   displayedColumns: string[] = ['id', 'message', 'name','createdate','updatedate','action'];
+//   dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+
+//   @ViewChild(MatPaginator)
+//   paginator!: MatPaginator;
+//   @ViewChild(MatSort) sort!: MatSort;
+// @ViewChild(MatCell) datesort!: MatCell;
+
+  
+
+//   ngAfterViewInit() {
+//     this.dataSource.paginator = this.paginator;
+//     this.dataSource.sort = this.sort;
+    
+// }
+// ngOnInit(): void {
+//   this.sorted()
+//   // this.http.get('https://redbricks-server.herokuapp.com/logs/proposal-log').subscribe((result: any) => {
+//   //   this.dataSource = result;
+//   //   console.log('DataSource::',this.dataSource);
+//   //   console.log('Result::',result);
+//   // })
+// }
+//   applyFilter(event: Event) {
+//     const filterValue = (event.target as HTMLInputElement).value;
+//     this.dataSource.filter = filterValue.trim().toLowerCase();
+  
+//   }
+//   announceSortChange(sortState: Sort) {
+//     if (sortState.direction) {
+//       this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+//     } else {
+//       this._liveAnnouncer.announce('Sorting cleared');
+//     }
+//   }
+
+//   // getExpiredOrNot = (currentElementdate: string,i: any) => {
+//   //   let current = new Date();
+//   //   let currnetMonth = current.getMonth();
+//   //   let currentDate = current.getDate();
+//   //   let elementDate = new Date(currentElementdate);
+//   //     let month = elementDate.getMonth();
+//   //     let date = elementDate.getDate();
+//   //     console.group(i);
+//   //     console.log("Date:",currentDate);
+//   //     console.log(current.toLocaleDateString())
+//   //     console.log(elementDate.toLocaleDateString())
+//   //     if((currnetMonth -2) >= month && currentDate > date){
+//   //       console.log(true)
+//   //       console.groupEnd();
+//   //       return true
+//   //     }
+//   //     else{
+//   //       console.log(false)
+//   //       console.groupEnd();
+//   //       return false
+//   //     }
+//   //   console.log(ELEMENT_DATA);
+//   // }
+
+//   sorted(){
+   
+//     ELEMENT_DATA.forEach((element:any)=>{
+//       console.log(element.createdate);
+//               const date =  new Date(element.createdate);
+//               const current = new Date();
+            
+//               var diff = current.getTime() - date.getTime(); 
+//               var daydiff = diff / (1000 * 60 * 60 * 24); 
+//                 console.log("difference",daydiff);
+//              if(daydiff>90)
+//              {
+//               this.change=true;
+//                console.log("expired");
+//              }else{
+//               this.change=false;
+//                console.log(" not expired");
+//              }
+//     });
+             
+            
+//   }
+// }
