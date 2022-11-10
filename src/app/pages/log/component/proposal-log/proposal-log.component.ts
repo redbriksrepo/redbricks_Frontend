2b8b9593a37529cae047d8532f02845b81af8704
vikrant29k <116786939+vikrant29k@users.noreply.github.com>
2022-11-10 14:23:41 +0530

    Desktop responive change in css

--- src/app/pages/log/component/proposal-log/proposal-log.component.ts
+++ src/app/pages/log/component/proposal-log/proposal-log.component.ts
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import { ViewChild } from "@angular/core";
 import { Component, OnInit } from "@angular/core";
 import { MatPaginator } from "@angular/material/paginator";
@@ -6,22 +7,36 @@ import { MatTable, MatTableDataSource } from "@angular/material/table";
 import { LogService } from "src/app/service/log/log.service";
 import { Router } from "@angular/router";
 
-export interface ProposalLogData {
-    _id: any;
-    proposalId: string;
-    clientName: string;
-    logMessage: string;
-    createdAt: Date;
-    updatedAt: Date;
+=======
+import { ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTable, MatTableDataSource } from '@angular/material/table';
+import { LogService } from 'src/app/service/log/log.service';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { all } from 'macaddress';
+import { Pipe, PipeTransform } from '@angular/core';
 
+import { FormControl, FormGroup } from '@angular/forms';
+>>>>>>> Stashed changes
+export interface ProposalLogData {
+  _id: any;
+  proposalId: string;
+  clientName: string;
+  logMessage: string;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
 @Component({
-    selector: 'log-proposal-log',
-    templateUrl: './proposal-log.component.html',
-    styleUrls: ['./proposal-log.component.scss']
+  selector: 'log-proposal-log',
+  templateUrl: './proposal-log.component.html',
+  styleUrls: ['./proposal-log.component.scss'],
 })
 export class LogProposalLog implements OnInit {
+<<<<<<< Updated upstream
 
     displayedColumns: string[] = ['proposalId', 'logMessage', 'clientName', 'date', 'action'];
     dataSource!: MatTableDataSource<ProposalLogData>;
@@ -62,8 +77,125 @@ export class LogProposalLog implements OnInit {
         this.dataSource.paginator = this.paginator;
         this.table.renderRows();
     }
+=======
+  logData: any;
 
-    viewDetails = (Id: string) => {
-        this.router.navigate(['/new-proposal', 'proposal-preview', Id]);
+  displayedColumns: string[] = [
+    'proposalId',
+    'logMessage',
+    'clientName',
+    'date',
+    'action',
+  ];
+  dataSource!: MatTableDataSource<ProposalLogData>;
+
+  @ViewChild(MatTable) table!: MatTable<ProposalLogData>;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  arr = this.logService.getAllLogs().subscribe;
+  constructor(
+    private logService: LogService,
+    private router: Router,
+    private http: HttpClient
+  ) {}
+
+  ngOnInit(): void {
+    this.getAllLogs();
+    // this.readntransform();
+  }
+  getExpiredOrNot = (date: any) => {
+    date = new Date(date);
+    let current = new Date();
+    let diff = current.getTime() - date.getTime();
+    let dayDiff = diff / (1000 * 60 * 60 * 24);
+
+    if (dayDiff > 90) {
+      return true;
+    } else {
+      return false;
     }
+  };
+  // readntransform(){
+  //    // let data;
+  //     this.http.get('https://redbricks-server.herokuapp.com/logs/proposal-log').subscribe((res:any)=>{
+  //        let a = res.map((d:any) => {
+  //         let data = d;
+  //         data = {...data,expired: this.getExpiredOrNot(data.createdAt)};
+  //         return data;
+  //        });
+
+  //     });
+
+  // }
+
+  updatetable = () => {};
+
+  applyFilter = (event: Event) => {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  };
+  sortdate = (filter: string) => {
+    if (filter === 'expired') {
+      this.displayExpiredData();
+    } else if (filter === 'not') {
+      this.displayNotExpiredData();
+    } else {
+      this.tableDataSource(this.logData);
+    }
+  };
+  displayAllData = (result: any) => {
+    let a = result.map((d: any) => {
+      let data = d;
+      data = { ...data, expired: this.getExpiredOrNot(data.createdAt) };
+      return data;
+    });
+    return a;
+  };
+  displayExpiredData = async () => {
+    let a: any = [];
+    this.logData.forEach((d: any) => {
+      if (d.expired === true) {
+        a = [...a, d];
+      }
+    });
+    console.log(a);
+    this.tableDataSource(a);
+  };
+  displayNotExpiredData = async () => {
+    let a: any = [];
+    this.logData.forEach((d: any) => {
+      if (d.expired === false) {
+        a = [...a, d];
+      }
+    });
+    console.log(a);
+    this.tableDataSource(a);
+  };
+
+  getAllLogs = (filter: string = 'all') => {
+    this.logService.getAllLogs().subscribe({
+      next: (result: any) => {
+        let a = this.displayAllData(result);
+        // console.log(result);
+        this.logData = [...a];
+
+        this.tableDataSource(a);
+      },
+    });
+  };
+>>>>>>> Stashed changes
+
+  tableDataSource = (data: any) => {
+    this.dataSource = new MatTableDataSource(data);
+    this.dataSource.paginator = this.paginator;
+    // this.table.renderRows();
+  };
+
+  viewDetails = (Id: string) => {
+    this.router.navigate(['/new-proposal', 'proposal-preview', Id]);
+  };
 }
